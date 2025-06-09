d4bf40b14a4a08fa9ec6d6a7e21bee10576517f3
meet4171 <meet45jadav@gmail.com>
2025-06-09 17:59:27 +0530

    updated code

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+// export const APP_MODE = "development";
 export const APP_MODE = "production";
 
 export const BASE_URL = APP_MODE === "production"
-    ? "https://www.meetjadav.shop"
+    ? "https://meetjadav.shop"
     : "http://localhost:3000";
